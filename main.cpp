977d1f09278cab7b4808882b63dfbddccfdd4d39
Jianru Lin <jianru.lin@gmail.com>
2011-09-25 00:07:36 +0800

    much simpler to understand now.

--- main.cpp
+++ main.cpp
@@ -5,97 +5,120 @@
 #include "util.h"
 #include "handler.h"
 
+//////////////////////////////////////////////////////////////////////////
+// 数据类型定义
+//////////////////////////////////////////////////////////////////////////
+
 typedef void (*FunctionHandler)(void);
 
 typedef struct _FunctionEntry
 {
-	char * declaration;
+	char * description;
 	FunctionHandler handler;
 } FunctionEntry;
+
+//////////////////////////////////////////////////////////////////////////
+// 全局变量定义
+//////////////////////////////////////////////////////////////////////////
 
 FunctionEntry * pEntrys = NULL;
 const int count = 24;
 
-// 内存分配函数
-// 其实是对 calloc 的封装
-// 之所以要再封装，是在 calloc 的基础上定义了内存分配失败时的处理策略
-// 如果内存不足，立刻结束程序，不再继续执行
-// 这是比较粗糙的处理手法，但是可以保证不会再发生其他致命错误
-// 在本项目中不涉及用户数据的保存，因此粗暴的结束程序
-// 并不会带来数据丢失等问题
-void * memoryAllocate(size_t size)
+//////////////////////////////////////////////////////////////////////////
+// 函数原型声明
+//////////////////////////////////////////////////////////////////////////
+
+// 初始化，所有条目都会在这里面添加
+void initialize();
+// 添加一条新条目
+void addEntry(char * description, FunctionHandler handler);
+// 设置地 i 个条目的内容
+void setEntry(int i, const char * description, FunctionHandler handler);
+// 把所有条目显示在屏幕上
+void showEntrys();
+// 与用户进行一次交互（让用户选择，然后执行）
+bool interactWithUser();
+
+
+//////////////////////////////////////////////////////////////////////////
+// 函数实现
+//////////////////////////////////////////////////////////////////////////
+
+int main()
 {
-	void * p = calloc(1, size);
-	if (p == NULL)
-	{
-		puts("allocate memory failed!\n");
-		exit(0);
-	}
-	else
-	{
-		return p;
-	}
+	// 首先进行初始化
+	// 然后不停的与用户进行交互
+	// 直到用户要求退出
+	initialize();
+	while(interactWithUser()){}
+	return 0;
 }
 
-
-void setEntry(FunctionEntry * pEntry, char * declaration, FunctionHandler handler)
+void setEntry(int index, const char * description, FunctionHandler handler)
 {
-	// 三个参数都必须给出，不能为空
-	assert(pEntry);
-	assert(declaration);
-	assert(handler);
+	assert(index > -1 && index < count);
+	FunctionEntry * pEntry = &pEntrys[index];
 
 	// 如果 pEntry 的 declaration 目前是有内容的，先将该内容删除
 	// 然后再替换为新内容 （涉及内存的释放与再分配）
-	if (pEntry->declaration != NULL)
+	if (pEntry->description != NULL)
 	{
-		free(pEntry->declaration);
-		pEntry->declaration = NULL;
+		free(pEntry->description);
+		pEntry->description = NULL;
 	}
 
-	pEntry->declaration = (char *)memoryAllocate(strlen(declaration) + 1);
-	strcpy(pEntry->declaration, declaration);
+	// 新内容可以是 NULL
+	// 如果不是 NULL 则要分配合适大小的内存
+	// 并进行字符串拷贝
+	if (description != NULL)
+	{
+		pEntry->description = (char *)memoryAllocate(strlen(description) + 1);
+		strcpy(pEntry->description, description);
+	}
 
 	// 函数指针无需释放，因此无乱现在是有值还是无值
 	// 直接设置为新的值即可
+	// 新的值也可以是 NULL
 	pEntry->handler = handler;
 }
 
-void createEntry(int index, char * declaration, FunctionHandler handler)
+void addEntry(char * description, FunctionHandler handler)
 {
+	static int index = 0;
 	assert(index < count);
-	assert(declaration);
-	assert(handler);
-	setEntry(&pEntrys[index], declaration, handler);
+	setEntry(index++, description, handler);
 }
 
-void initEntrys()
+void initialize()
 {
+	// 为条目列表分配存储空间
 	pEntrys = (FunctionEntry *)memoryAllocate(sizeof(FunctionEntry) * count);
-	createEntry(0, "double acos(double x)", acos_handler);
-	createEntry(1, "double asin(double x)", asin_handler);
-	createEntry(2, "double atan(double x)", atan_handler);
-	createEntry(3, "double cos(double x)", cos_handler);
-	createEntry(4, "double sin(double x)", sin_handler);
-	createEntry(5, "double tan(double x)", tan_handler);
-	createEntry(6, "double cosh(double x)", cosh_handler);
-	createEntry(7, "double sinh(double x)", sinh_handler);
-	createEntry(8, "double tanh(double x)", tanh_handler);
-	createEntry(9, "double atan2(double x, double y)", atan2_handler);
-	createEntry(10, "double fabs(double x)", fabs_handler);
-	createEntry(11, "double abs(int x)", abs_handler);
-	createEntry(12, "double ceil(double x)", ceil_handler);
-	createEntry(13, "double floor(double x)", floor_handler);
-	createEntry(14, "double exp(double x)", exp_handler);
-	createEntry(15, "double fmod(double x, double y)", fmod_handler);
-	createEntry(16, "double frexp(double x, int *exp)", frexp_handler);
-	createEntry(17, "double hpyot(double x, double y)", hypot_handler);
-	createEntry(18, "double ldexp(double x, int exp)", ldexp_handler);
-	createEntry(19, "double log(double x)", log_handler);
-	createEntry(20, "double log10(double x)", log10_handler);
-	createEntry(21, "double modf(double x, double *i)", modf_handler);
-	createEntry(22, "double pow(double x, double y)", pow_handler);
-	createEntry(23, "double sqrt(double x)", sqrt_handler);
+
+	// 添加各个条目，注意条目数目要和内存空间吻合，不多不少
+	addEntry("double acos(double x)", acos_handler);
+	addEntry("double asin(double x)", asin_handler);
+	addEntry("double atan(double x)", atan_handler);
+	addEntry("double cos(double x)", cos_handler);
+	addEntry("double sin(double x)", sin_handler);
+	addEntry("double tan(double x)", tan_handler);
+	addEntry("double cosh(double x)", cosh_handler);
+	addEntry("double sinh(double x)", sinh_handler);
+	addEntry("double tanh(double x)", tanh_handler);
+	addEntry("double atan2(double x, double y)", atan2_handler);
+	addEntry("double fabs(double x)", fabs_handler);
+	addEntry("double abs(int x)", abs_handler);
+	addEntry("double ceil(double x)", ceil_handler);
+	addEntry("double floor(double x)", floor_handler);
+	addEntry("double exp(double x)", exp_handler);
+	addEntry("double fmod(double x, double y)", fmod_handler);
+	addEntry("double frexp(double x, int *exp)", frexp_handler);
+	addEntry("double hpyot(double x, double y)", hypot_handler);
+	addEntry("double ldexp(double x, int exp)", ldexp_handler);
+	addEntry("double log(double x)", log_handler);
+	addEntry("double log10(double x)", log10_handler);
+	addEntry("double modf(double x, double *i)", modf_handler);
+	addEntry("double pow(double x, double y)", pow_handler);
+	addEntry("double sqrt(double x)", sqrt_handler);
 }
 
 void showEntrys()
@@ -103,37 +126,30 @@ void showEntrys()
 	assert(pEntrys);
 	for (int i = 0; i < count; i++)
 	{
-		printf("%d %s\n", i, pEntrys[i].declaration);
+		printf("%d %s\n", i, pEntrys[i].description);
 	}
 }
 
-void begin()
+bool interactWithUser()
 {
-	do 
+	printf("function list:\n");
+	showEntrys();
+	int i = getInt("select (-1 for quit): ");
+	if (i == -1)
 	{
-		puts("function list:");
-		showEntrys();
-		int i = getInt("select (-1 for quit): ");
-		if (i == -1)
-		{
-			break;
-		}
-		else if (i > -1 && i < count)
-		{
-			pEntrys[i].handler();
-		}
-		else
-		{
-			printf("index out of range [%d, %d]\n", 0, count - 1);
-		}
+		return false;
+	}
+	else if (i > -1 && i < count)
+	{
+		pEntrys[i].handler();
 		system("pause");
 		system("cls");
-	} while (true);
-}
-
-int main()
-{
-	initEntrys();
-	begin();
-	return 0;
+	}
+	else
+	{
+		printf("index out of range [%d, %d]\n", 0, count - 1);
+		system("pause");
+		system("cls");
+	}
+	return true;
 }
--- util.h
+++ util.h
@@ -5,3 +5,12 @@ int getInt(const char * message);
 
 // 让用户输入一个 double 型的数字
 double getDouble(const char * message);
+
+// 内存分配函数
+// 其实是对 calloc 的封装
+// 之所以要再封装，是在 calloc 的基础上定义了内存分配失败时的处理策略
+// 如果内存不足，立刻结束程序，不再继续执行
+// 这是比较粗糙的处理手法，但是可以保证不会再发生其他致命错误
+// 在本项目中不涉及用户数据的保存，因此粗暴的结束程序
+// 并不会带来数据丢失等问题
+void * memoryAllocate(size_t size);
